eace10743a55b232be8a5c5506b586b7d406d182
Jaeyoung Bang <qkdwo4613@gmail.com>
2023-04-25 19:57:55 +0900

    Add SpringSecurityConfig.java
    need to add more details later.

---
+++ SpringSecurityConfig.java
@@ -0,0 +1,68 @@
+package me.benny.practice.spring.security.config;
+
+import lombok.RequiredArgsConstructor;
+import me.benny.practice.spring.security.user.User;
+import me.benny.practice.spring.security.user.UserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+        http.csrf();
+        http.rememberMe();
+
+        // authorization
+        http.authorizeRequests()
+                .antMatchers("/", "/home", "/signup").permitAll()
+                .antMatchers("/note").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/notice").authenticated()
+                .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        // login
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll();
+        // logout
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userService.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException(username);
+            } else {
+                return user;
+            }
+        };
+    }
+}
